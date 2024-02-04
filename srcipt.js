1be942b99094a3bedeced1ffae6efb758eae7c3d
Tuyiseng <140953186+Tuyiseng@users.noreply.github.com>
2024-02-04 21:13:38 +0200

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,132 @@
+
+
+let house2=document.querySelectorAll('#house2');
+house2.forEach(function(element) {
+    element.classList.add('house2');
+});
+/*  house2.classList.add('house2');
+let pnim=document.querySelectorAll('.propertysty');
+
+pnim.forEach((e)=>{
+e.classList.add('pnim');
+
+});
+*/
+
+let img2=document.querySelectorAll('.img2');
+//img2.classList.add('styleImg2');
+img2.forEach((e)=>{
+e.classList.add('styleImg2');    
+});
+let houseimage=document.querySelectorAll('#houseimage');
+//houseimage.classList.add('size');
+houseimage.forEach((e)=>{
+    e.classList.add('size');
+});
+
+let imgdiv=document.querySelectorAll('#imgdiv');
+//imgdiv.classList.add('imgsiz');
+imgdiv.forEach((e)=>{
+    e.classList.add('imgsiz');
+  });
+
+let househead=document.querySelectorAll('#househead');
+//househead.classList.add('headanim');
+househead.forEach((e)=>{
+    e.classList.add('headanim');
+});
+
+let description=document.querySelector('.description');
+description.classList.add('styleDescription');
+
+
+let moredetails=document.querySelector('.moredetails');
+moredetails.classList.add('more');
+
+let housebutton=document.querySelectorAll('#housebutton');
+//housebutton.classList.add('housebuttong')
+//housebutton.classList.add('pnim')
+housebutton.forEach((e)=>{
+    e.classList.add('housebuttong');
+});
+
+let rentbutton=document.querySelectorAll('#rentbutton');
+//rentbutton.classList.add('rentb')
+//rentbutton.classList.add('pnim')
+rentbutton.forEach((e)=>{
+    e.classList.add('rentb');
+});
+
+let pnim=document.querySelectorAll('.propertysty');
+
+pnim.forEach((e)=>{
+e.classList.add('pnim');
+
+});
+ function logo(){
+
+
+
+let logoimg=document.querySelector("#logoImg");
+
+logoimg.classList.add("styleLogoImg");
+
+let logo=document.querySelector(".logo");
+logo.classList.add("styleLogo")
+
+
+let logoList=document.querySelector(".logoList");
+logoList.classList.add("styleLogoList");
+
+let logoHead=document.querySelector("#logoHead");
+logoHead.classList.add("styleLogoHead");
+
+let logoButton= document.querySelector(".logoButton");
+logoButton.classList.add("styleLogoButton");
+let Main=document.querySelector(".Maindiv");
+Main.classList.add("styleMain");
+ }
+ logo();
+
+
+let main=document.querySelector(".main");
+main.classList.add("styleMainDiv");
+
+main.classList.add("styleMain");
+let srch=document.querySelector(".srch");
+srch.classList.add("styleSrch");
+
+
+let fayoutube=document.querySelectorAll('.fa-youtube');
+fayoutube.forEach((e)=>{
+    e.classList.add('styleFaYoutube');
+});
+
+
+let fastar=document.querySelectorAll('.fa-star');
+fastar.forEach((e)=>{
+    e.classList.add('styleFastar');
+});
+
+
+let labelhead=document.querySelectorAll('.labelhead');
+labelhead.forEach((e)=>{
+    e.classList.add('styleLabelHead');
+});
+
+function endFunction()
+{
+    const end=document.querySelector('.end');
+    end.classList.add('styleEnd');
+
+    const end1=document.querySelector('.end1');
+   end1.classList.add('styleEnd1');
+
+    const end2=document.querySelector('.end2');
+    end2.classList.add('styleEnd2');
+   
+    const end3=document.querySelector('.end3');
+   end3.classList.add('styleEnd3');
+
+}
+endFunction()
